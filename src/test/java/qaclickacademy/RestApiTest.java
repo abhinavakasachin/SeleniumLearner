d60b164a1b7422b439b7f468bf3cf909ed048192
abhinav <abhinavakasachin@gmail.com>
2019-12-21 16:33:34 +0530

    commit from Asia

--- src/test/java/qaclickacademy/RestApiTest.java
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -9,11 +9,18 @@ public class RestApiTest {
 	public void PostJira()
 	{
 		System.out.println("PostJira");
+		System.out.println("PostJira2");
+		System.out.println("PostJira3");
+		System.out.println("PostJira4");
+		
 	}
 	
 	@Test
 	public void DeleteTwitter()
 	{
 		System.out.println("DeleteTwitter");
+		System.out.println("UpdateTwitter");
+		System.out.println("AddTwitter");
+		
 	}
 }
