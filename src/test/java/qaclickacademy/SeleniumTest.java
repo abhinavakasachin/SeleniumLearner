7c684255fe6b92049516a1bc9f90727be61ae4c8
abhinav <abhinavakasachin@gmail.com>
2019-12-21 15:48:06 +0530

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		//System.out.println("BrowserAutomation");
+		Assert.assertFalse(true);
+	}
+	
+	@Test
+	public void Elements()
+	{
+		System.out.println("A");
+	}
+
+}
